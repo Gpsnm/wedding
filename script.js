9bdb4d2292a6a67d37d8085e898aa1116f9cdaa7
adam willetts <adam-willetts@hotmail.co.uk>
2023-07-11 21:23:32 +0100

    fixed issue with navbar

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+let clicked = document.getElementById("myLinks");
+clicked.classList.toggle("addClass")
 }
